c15548afa46407c90769eb860f142fb03bf26f15
0shunya <shloksingh2805@gmail.com>
2025-08-19 03:03:30 +0530

    ClosureTestPart2/JSTestFinale

--- JSTestFinale/ClosureTest.js
+++ JSTestFinale/ClosureTest.js
@@ -11,3 +11,63 @@ let cc = createCounter();
 console.log(cc());
 console.log(cc());
 console.log(cc());
+
+
+function rateLimiter(fn, limit) {
+    let count = 0;
+    let name;
+
+    return function (...args) {
+        if (count < limit) {
+            count++;
+            fn(...args) //Prints Hello, this is shlok" 1st and then zafar
+            console.log(`Hello, this is ${name}`);
+            console.log("we passed if");
+        }
+        else {
+            console.log("Limit reached");
+            
+        }
+    }
+}
+
+function greet(name) {
+    console.log("Hello, this is " + name);
+}
+
+let start = rateLimiter(greet, 2);
+
+start("shlok");
+start("Zafar");
+start("Amir");
+
+
+function memoize(fn) {
+
+    const cache = {
+
+    }
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if(cache[key]) {
+            console.log("Fecthing from cache..");
+            return cache[key]
+        }
+        else {
+            const result = fn(...args);
+            cache[key] = result;
+            return result;
+        }
+
+
+    }
+
+
+} 
+
+let memo =  memoize((n) => n*n);
+console.log(memo(5));
+console.log(memo(5));
+console.log(memo(15));
