181effee6a3867d1c26cdac7b14f3b9781ab96cc
0shunya <shloksingh2805@gmail.com>
2025-08-27 17:47:12 +0530

    weatherJS

--- js-projects/02_weather/script.js
+++ js-projects/02_weather/script.js
@@ -1,58 +1,85 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cityInput = document.getElementById("city-input");
-  const getWeatherBtn = document.getElementById("get-weather-btn");
-  const weatherInfo = document.getElementById("weather-info");
-  const cityNameDisplay = document.getElementById("city-name");
-  const temperatureDisplay = document.getElementById("temperature");
-  const descriptionDisplay = document.getElementById("description");
-  const errorMessage = document.getElementById("error-messag");
+document.addEventListener('DOMContentLoaded', () => {
+  const CityINput = document.getElementById('city-input')
+  const BTNGetWeather = document.getElementById('get-weather-btn')
+  const WheathInfo = document.getElementById('weather-info')
+  const DSPCityName = document.getElementById('city-name')
+  const DSPCityTemp = document.getElementById('temperature')
+  const DSPCityDesc = document.getElementById('description')
+  const Err = document.getElementById('error-message')
 
-  const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"; //env variables
+  const API_KEY = "7017e333083dd28783293e8a5ef9c90d"
 
-  getWeatherBtn.addEventListener("click", async () => {
-    const city = cityInput.value.trim();
-    if (!city) return;
+  const btn = BTNGetWeather.addEventListener('click', async () => {
+    const city = CityINput.value.trim()
 
-    // it may throw an error
-    // server/database is always in another continent
+    if(!city) return;
+
+    //It may throw an error 
+    //Server/DB are always in another planet
 
     try {
-      const weatherData = await fetchWeatherData(city);
-      displayWeatherData(weatherData);
-    } catch (error) {
-      showError();
+     const weatherdata = await fetchweatherdata(city)
+     dspweatherdata(weatherdata)
+    //  delbtn()
+    } catch(error) {
+        dspError()
     }
-  });
+  
+  
+})
 
-  async function fetchWeatherData(city) {
-    //gets the data
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+// function delbtn(){
+//   if(BTNGetWeather.textContent = "Delete") {
+//       BTNGetWeather.style.backgroundColor = "red"
+//       BTNGetWeather.addEventListener('click', async () => {
+//         CityINput.value = "";
+//         DSPCityDesc.textContent = "";
+//         DSPCityName.textContent = "";
+//         DSPCityTemp.textContent = "";
+//       })
+//      }
+// }
 
-    const response = await fetch(url);
-    console.log(typeof response);
-    console.log("RESPONSE", response);
+async function fetchweatherdata(city) {
+  //get data
 
-    if (!response.ok) {
-      throw new Error(" City Not found");
-    }
-    const data = await response.json();
-    return data;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+
+  const response = await fetch(url)
+
+  console.log("Response", response)
+  console.log(typeof response)
+
+  if(!response.ok) {
+    CityINput.value = "";
+    throw new Error("City not found");    
   }
+     const data = await response.json();
+     return data;
+}
 
-  function displayWeatherData(data) {
-    console.log(data);
-    const { name, main, weather } = data;
-    cityNameDisplay.textContent = name;
-    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
-    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
 
-    //unlock the display
-    weatherInfo.classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-  }
+function dspweatherdata(wdata){
+  console.log(wdata);
 
-  function showError() {
-    weatherInfo.classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-  }
+  const {name, sys:{country}, main, weather} = wdata;
+
+  DSPCityName.textContent = `City: ${name}`;
+  // DSPCityDesc.textContent = country;
+  DSPCityDesc.textContent = `Description: ${weather[0].description}`
+  DSPCityTemp.textContent = `Temp: ${main.temp}`;
+  
+  WheathInfo.classList.remove('hidden')
+  Err.classList.add('hidden')
+
+}
+
+function dspError() {
+  WheathInfo.classList.add('hidden')
+  Err.classList.remove('hidden')
+}
+
+
 });
+
+
